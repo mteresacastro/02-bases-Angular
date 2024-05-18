f6c0c6d7a63c2eeefb8017b498e8b4f2326d6b7d
mteresacastro <m.teresacastrogonzalez@gmail.com>
2024-05-18 23:29:22 +0200

    ngFor

--- src/app/heroes/hero/hero.component.ts
+++ src/app/heroes/hero/hero.component.ts
@@ -9,4 +9,25 @@ export class HeroComponent {
 
   public name: string = 'ironman';
   public age:  number = 45;
+
+  get capitalizeName(): string{ // el get transforma el método en una propiedad
+    return this.name.toUpperCase()
+  };
+
+  getHeroDescription(): string{
+    return `${this.name} - ${this.age}`;
+  };
+
+  changeName():void{
+    this.name = 'Spiderman';
+  };
+
+  changeAge():void{
+    this.age = 18;
+  };
+
+  resetForm():void{
+    this.name = 'ironman'
+    this.age = 45;
+  };
 }
