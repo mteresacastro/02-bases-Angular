7aa54d4cda330a98762293642beb2182f626473c
mteresacastro <m.teresacastrogonzalez@gmail.com>
2024-05-18 14:45:54 +0200

    first steps

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
 })
+
 export class AppComponent {
   title = 'bases';
+  counter = 20;
+
+  increaseBy(value: number):void {
+    this.counter += value;
+  };
+
+  resetCounter():void {
+    this.counter = 20;
+  }
 }
